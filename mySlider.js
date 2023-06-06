e28ab3765c8869aafff2fdf4d97adb579a320a0d
Uroš Meh <uros.meh@pro-bit.si>
2023-06-06 22:19:07 +0200

    first version of slider values (fixed position)

--- mySlider.js
+++ mySlider.js
@@ -1,23 +1,27 @@
 class CircularSlider {
-    constructor({container, color, max, min, step, value, radius}) {
+    //todo: try to position slider in such way that legend will be visible
+    constructor({container, color, max, min, step, radius, title}) {
         this.container = document.getElementById(container);
+        console.log((this.container))
         this.color = color;
         this.max = max;
         this.min = min;
         this.step = step;
-        this.value = value || 0;
-        this.radius = radius
+        this.value = min;
+        this.radius = radius;
+        this.title = title;
         // this.svgSize = radius * 2 + 30;
         this.center = 300;
         this.elementId = Math.round(Math.random() * 1000);
-        this.parentSvg = this.container.querySelector("svg");
+        this.parentSvg = this.container.querySelector(".parent-svg");
+        this.valuesSvg = this.container.querySelector(".values-svg");
 
-
+        const rect = this.container.getBoundingClientRect();
         if (!this.parentSvg) {
             this.parentSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
             this.parentSvg.setAttribute("class", "parent-svg")
-            this.parentSvg.setAttribute("width", 600); //set this to max possible width and height
-            this.parentSvg.setAttribute("height", 600);
+            this.parentSvg.setAttribute("width", rect.width); //set this to max possible width and height
+            this.parentSvg.setAttribute("height", rect.height);
             this.container.appendChild(this.parentSvg);
         }
 
@@ -30,14 +34,53 @@ class CircularSlider {
         this.renderElements();
         this.renderSteps();
         this.attachEventListeners();
+        this.repositionTexts();
+        // console.log(this.getMaxRadius());
     }
 
     renderElements() {
-        let svg = document.createElementNS("http://www.w3.org/2000/svg", "g");
-        svg.setAttribute("id", `svg-${this.elementId}`);
+        const existingValueElements = this.parentSvg.querySelectorAll(".value-text");
+        const elementDy = existingValueElements.length * 30 + 300;
+        // const flexContainer = document.createElement("div");
+        // flexContainer.style.display = "flex";
+        // flexContainer.style.flexDirection = "row";
+        // flexContainer.style.gap = "10px";
+    
+        const valueGroup = document.createElementNS("http://www.w3.org/2000/svg", "g");
+        valueGroup.setAttribute("class", "value-text");
+
+        const valueElement = document.createElementNS("http://www.w3.org/2000/svg", "text");
+        valueElement.setAttribute("dy", elementDy.toString())
+        valueElement.setAttribute("id", `value-${this.elementId}`);
+        valueElement.style.fontSize = "30px";
+        valueElement.textContent = this.value;
+        valueGroup.appendChild(valueElement);
+
+        const valueLegendBox = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+        valueLegendBox.setAttribute("width", "25");
+        valueLegendBox.setAttribute("height", "10");
+        valueLegendBox.setAttribute("fill", this.color);
+        valueLegendBox.setAttribute("transform", `translate(50, ${elementDy - 10})`);
+        valueGroup.appendChild(valueLegendBox);
+
+        const valueTitle = document.createElementNS("http://www.w3.org/2000/svg", "text");
+        valueTitle.setAttribute("dy", elementDy);
+        valueTitle.setAttribute("dx", "80");
+        valueTitle.setAttribute("id", `value-${this.elementId}`);
+        valueTitle.style.fontSize = "20px";
+        valueTitle.textContent = this.title;
+        valueGroup.appendChild(valueTitle);
+
+        // flexContainer.appendChild(valueGroup);
+
+        this.parentSvg.appendChild(valueGroup);
+
+        const svg = document.createElementNS("http://www.w3.org/2000/svg", "g");
+        svg.setAttribute("class", "svg-container");
 
         const outerCircle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
         outerCircle.setAttribute("id", `outer-circle-${this.elementId}`)
+        outerCircle.setAttribute("class", "outer-circle")
         outerCircle.setAttribute("cx", 300);
         outerCircle.setAttribute("cy", 300);
         outerCircle.setAttribute("r", this.radius);
@@ -45,19 +88,19 @@ class CircularSlider {
         outerCircle.setAttribute("stroke-width", "20");
         outerCircle.setAttribute("fill", "transparent");
 
-        let progressPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
+        const progressPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
         progressPath.setAttribute("id", `progress-${this.elementId}`);
         progressPath.setAttribute("d", "");
-        progressPath.setAttribute("stroke", "magenta");
+        progressPath.setAttribute("stroke", this.color);
         progressPath.setAttribute("stroke-width", "20");
         progressPath.setAttribute("fill", "transparent");
 
-        let stepsGroup = document.createElementNS("http://www.w3.org/2000/svg", "g");
+        const stepsGroup = document.createElementNS("http://www.w3.org/2000/svg", "g");
         stepsGroup.setAttribute("id", `steps-${this.elementId}`);
         stepsGroup.setAttribute("stroke", "white");
         stepsGroup.setAttribute("stroke-width", "1");
 
-        let handleCircle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+        const handleCircle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
         handleCircle.setAttribute("id", `handle-${this.elementId}`);
         handleCircle.setAttribute("cx", 300);
         handleCircle.setAttribute("cy", (300 - this.radius));
@@ -98,9 +141,11 @@ class CircularSlider {
 
     attachEventListeners() {
         this.parentSvg.addEventListener('mousedown', (e) => {
+            console.log(e.target.id)
             if (this.isEventOnSliderElement(e)) {
                 this.isMouseDown = true;
                 this.startAngle = this.calculateAngle(e);
+                console.log(`startAngle: ${this.startAngle}`)
                 this.updateProgress(e);
             }
         });
@@ -154,6 +199,16 @@ class CircularSlider {
         const x = e.clientX - rect.left - 300;
         const y = e.clientY - rect.top - 300;
 
+        //radius: 80
+        const distance  = Math.sqrt(x*x + y*y);
+        if (!this.isMouseDown && !(distance > this.radius - 20/2) && !(distance < this.radius + 20/2)) {
+            console.log("asf")
+            return;
+        }
+
+
+        console.log(`${e.target.id}: x(${x}), y(${y})`)
+
         const radian = Math.atan2(y, x);
         const endX = 300 + Math.cos(radian) * this.radius;
         const endY = 300 + Math.sin(radian) * this.radius;
@@ -190,11 +245,29 @@ class CircularSlider {
 
         const val = this.min + currentAngle / (2 * Math.PI) * (this.max - this.min);
         this.value = Math.round(val / this.step) * this.step;
-        console.log(this.value);
+        this.parentSvg.querySelector(`#value-${this.elementId}`).textContent = this.value;
     }
 
     isEventOnSliderElement(e) {
         return e.target.id === `handle-${this.elementId}` || e.target.id === `outer-circle-${this.elementId}` || e.target.id === `progress-${this.elementId}` || e.target.classList.contains(`line-${this.elementId}`);
     }
 
+    getMaxRadius() {
+        const sliders = this.parentSvg.querySelectorAll(".outer-circle");
+        let maxRadius = 0;
+
+        sliders.forEach(s => {
+            const radius = parseInt(s.getAttribute("r"));
+            if (radius > maxRadius) {
+                maxRadius = radius;
+            }
+        });
+
+        return maxRadius;
+    }
+
+    repositionTexts() {
+        const textElements = this.parentSvg.querySelector(".value-text");
+        console.log(textElements)
+    }
 }
